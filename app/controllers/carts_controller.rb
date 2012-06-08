305609004bf75dbae4087de9601d6106573cc4ec
Gennaro Piano <pianogen@students.zhaw.ch>
2012-06-08 23:04:12 +0200

    Chapter 11 finished

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,17 +13,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-	begin
-		@cart = Cart.find(params[:id])
-	rescue ActiveRecord::RecordNotFound
-		logger.error "Attempt to access invalid cart #{params[:id]}"
-		redirect_to store_url, notice: 'Invalid cart'
-	else
-		respond_to do |format|
-			format.html # show.html.erb
-			format.json { render json: @cart }
+		begin
+			@cart = Cart.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			logger.error "Attempt to access invalid cart #{params[:id]}"
+			redirect_to store_url, notice: 'Invalid cart'
+		else
+			respond_to do |format|
+				format.html # show.html.erb
+				format.json { render json: @cart }
+			end
 		end
-    end
   end
 
   # GET /carts/new
@@ -79,10 +79,11 @@ class CartsController < ApplicationController
   def destroy
     @cart = current_cart
     @cart.destroy
-	session[:cart_id] = nil
-	respond_to do |format|
-		format.html { redirect_to store_url }
-		format.json { head :ok }
+		session[:cart_id] = nil
+
+    respond_to do |format|
+      format.html { redirect_to store_url }
+      format.json { head :no_content }
     end
   end
 end
--- test/functional/carts_controller_test.rb
+++ test/functional/carts_controller_test.rb
@@ -41,7 +41,7 @@ class CartsControllerTest < ActionController::TestCase
 
   test "should destroy cart" do
     assert_difference('Cart.count', -1) do
-		sesion[:cart_id] = @cart.id
+		session[:cart_id] = @cart.id
 		delete :destroy, id: @cart
     end
     assert_redirected_to store_path
